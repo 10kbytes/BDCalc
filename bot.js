d64edb67ec8b04600f24ead501856fc1540b93ec
10kbytes <44127598+10kbytes@users.noreply.github.com>
2018-10-14 10:58:08 +0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+			//zeln
+			case 'zeln':
+				bot.sendMessage({
+					to: channelID,
+					message: 'Zeln is Gay!!'
+				});
+			
+         }
+     }
+});
+
+client.login(process.env.BOT_TOKEN)
